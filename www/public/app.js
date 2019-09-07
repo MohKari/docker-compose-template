cf31c9b6cde4c18add761fa104bb63d2531c5e67
LawrenceMiller1976 <lawrence_e_miller@hotmail.com>
2019-09-07 20:36:31 +0100

    Added some HTML trying to get the form data to the script, not got it so far.

--- www/public/app.js
+++ www/public/app.js
@@ -2,4 +2,5 @@
 //Generate the random number and test with console.log.
 //I missed the brackets off Math.random caused all manner of head scratching.
 var secret = Math.floor((Math.random())  * 100 + 1);
-console.log(secret);
+var guess = document.getElementById(guess);
+console.log(guess);
