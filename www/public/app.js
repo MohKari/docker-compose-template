2622f4713615dde02b64046d557a1c8d1c24e41f
LawrenceMiller1976 <lawrence_e_miller@hotmail.com>
2019-09-07 21:56:32 +0100

    no message

--- www/public/app.js
+++ www/public/app.js
@@ -2,5 +2,4 @@
 //Generate the random number and test with console.log.
 //I missed the brackets off Math.random caused all manner of head scratching.
 var secret = Math.floor((Math.random())  * 100 + 1);
-var guess = document.getElementById(guess);
-console.log(guess);
+console.log(secret);
