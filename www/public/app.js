761e91428441a3bad8afbecc260952d663d982de
LawrenceMiller1976 <lawrence_e_miller@hotmail.com>
2019-09-07 16:26:08 +0100

    Started some coding, not a lot due to some basic errors, but it's a start. Will try to do more later.

---
+++ www/public/app.js
@@ -0,0 +1,5 @@
+//Kurgamemnon 07092019
+//Generate the random number and test with console.log.
+//I missed the brackets off Math.random caused all manner of head scratching.
+var secret = Math.floor((Math.random())  * 100 + 1);
+console.log(secret);
