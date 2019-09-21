e5140729903d2507f35ef9a0bc6cf29aac2f83f0
LawrenceMiller1976 <lawrence_e_miller@hotmail.com>
2019-09-21 17:30:15 +0100

    Added game mechanics. Tested and working.

--- www/public/app.js
+++ www/public/app.js
@@ -5,4 +5,19 @@ var secret = Math.floor((Math.random())  * 100 + 1);
 //It's local scope so can't be accessed outside of the function.
 $("#button").click(function(){
         var guess = document.getElementById("userInput").valueAsNumber;
+    if(Number.isNaN(guess)){
+        document.getElementById("gameOutput").innerHTML = "That is not a number!";
+    }
+    else if (guess <=0 || guess >=101){
+        document.getElementById("gameOutput").innerHTML = "That number is not between 1 and 100!";
+    }
+    else if (guess > secret){
+        document.getElementById("gameOutput").innerHTML = "Too high plaese guess again";
+    }
+    else if (guess < secret){
+        document.getElementById("gameOutput").innerHTML = "Too low please guess again";
+    }
+    else if (guess = secret){
+        document.getElementById("gameOutput").innerHTML = "Well done you have guessed correctly";
+    }
 })
