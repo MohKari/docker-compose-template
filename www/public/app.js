68afe4316a846580300aaf02bda7f2880a0cb25c
LawrenceMiller1976 <lawrence_e_miller@hotmail.com>
2019-09-21 17:37:16 +0100

    Added coments to script.

--- www/public/app.js
+++ www/public/app.js
@@ -5,12 +5,14 @@ var secret = Math.floor((Math.random())  * 100 + 1);
 //It's local scope so can't be accessed outside of the function.
 $("#button").click(function(){
         var guess = document.getElementById("userInput").valueAsNumber;
+    //Check if number is a valid number between 1 and 100
     if(Number.isNaN(guess)){
         document.getElementById("gameOutput").innerHTML = "That is not a number!";
     }
     else if (guess <=0 || guess >=101){
         document.getElementById("gameOutput").innerHTML = "That number is not between 1 and 100!";
     }
+    //Compare guess to secret and give user output.
     else if (guess > secret){
         document.getElementById("gameOutput").innerHTML = "Too high plaese guess again";
     }
